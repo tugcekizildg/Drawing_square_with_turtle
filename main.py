4873c2012419880cdfb80d39a373af3436939071
Tuğçe Kızıldağ <141547888+tugcekizildg@users.noreply.github.com>
2023-10-24 13:46:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import turtle
+import tkinter
+
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("medium purple")
+drawing_board.title("Drawing Square")
+turtle_instance = turtle.Turtle()
+
+turtle_instance.forward(300)
+turtle_instance.right(90)
+turtle_instance.forward(300)
+turtle_instance.right(90)
+turtle_instance.forward(300)
+turtle_instance.right(90)
+turtle_instance.forward(300)
+turtle.done()
